9c19916c25a912d18c8cef8c8fdf9e94cd2121f9
narerken <shakualyernar@gmail.com>
2025-12-18 22:34:25 +0500

    Initial spring boot project

---
+++ src/main/java/kz/narxoz/kordim/KordimApplication.java
@@ -0,0 +1,13 @@
+package kz.narxoz.kordim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KordimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KordimApplication.class, args);
+	}
+
+}
